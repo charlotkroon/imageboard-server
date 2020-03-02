ef3b34397262cf3cddb934236e4c20aa80c60143
charliegabriella <charlotkroon@gmail.com>
2020-03-02 14:00:29 +0100

    Imported sequelize

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+app.get("/data", (request, response) => {
+  response.send("some data");
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
