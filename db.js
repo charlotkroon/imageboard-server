9f93c4ef77b583211cba5f060402686fda77f781
charliegabriella <charlotkroon@gmail.com>
2020-03-02 15:52:11 +0100

    put the right database url in db.js

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
-const databaseUrl = "postgres://postgres:secret@localhost:5432/postgres";
-
+const databaseURL =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:secret@localhost:5432/postgres";
 const db = new Sequelize(databaseUrl);
 db.sync({ force: false })
   .then(() => console.log("Database schema updated"))
